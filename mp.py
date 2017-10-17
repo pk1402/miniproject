5ab5f04fa4154886fef59e5e4296e95cf47bf8ff
pk1402 <praveen.kumarporumalla@dal.ca>
2017-10-17 07:31:48 -0300

    test commit

--- mp.py
+++ mp.py
@@ -1,2 +1,8 @@
-print ("test commit")
+#print ("test commit
+sudo pip install requests
+import requests 
+main_api='https://opendata.arcgis.com/datasets/3df29a3d088a42d890f11d027ea1c0be_0.geojson'
+json_data =requests.get(main_api).json()
+formatted_address=json_data['features'][0]['properties']['park_name']
+print('formatted_address')
 
