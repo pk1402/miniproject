0ca4378a78da31fcbbd4efd012754c52b4781014
pk1402 <praveen.kumarporumalla@dal.ca>
2017-10-17 07:39:50 -0300

    installed pip and requests sucessfully

--- mp.py
+++ mp.py
@@ -1,5 +1,5 @@
 #print ("test commit
-sudo pip install requests
+#sudo pip install requests
 import requests 
 main_api='https://opendata.arcgis.com/datasets/3df29a3d088a42d890f11d027ea1c0be_0.geojson'
 json_data =requests.get(main_api).json()
