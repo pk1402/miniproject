f509b04f9cf5c3eabca4a220903fceba20029b09
pk1402 <you@example.com>
2017-10-19 14:58:31 -0700

    finally printed parkname,lat &long, area

--- mp.py
+++ mp.py
@@ -12,28 +12,48 @@ json_data =requests.get(main_api).json()
 #po = a.get("properties")
 #e = po.get("PARK_NAME")
 #print(e)
-fid=[]
-cleanlist=[]
-for i in range(902):
- my_result = json_data['features'][i]['properties']['PARK_NAME']
+
+#fid=[]
+#cleanlist=[]
+#for i in range(902):
+ #my_result = json_data['features'][i]['properties']['PARK_NAME']
  #my_result2=json_data.get['feature']  
- fid.append(my_result)
-[cleanlist.append(x) for x in fid if x is not in cleanlist]
+ #fid.append(my_result,my_result2)
+#[cleanlist.append(x) for x in fid if x is not in cleanlist]
  #for i in fid:
   #print(i) 
   #print(type(a))
   #str1 = ''.join(a)
- print(my_result)
+ #print(my_result)
+fid=[] #empty list
+cleanlist=[]
+for i in range(902):
+    my_result = json_data['features'][i]['properties']['PARK_NAME'] #display park names in halifax
+    my_result2 = json_data['features'][i]['properties']['HECTARES'] #display the area of park
+    fid.append(my_result)
+    fid.append(my_result2)
+    #removing duplicates in the list
+    [cleanlist.append(x) for x in fid if x not in cleanlist]
+
+#second_api='https://maps.google.com/maps/api/geocode/json?'
+#for parks in cleanlist:
+
+    #url2=second_api+urllib.parse.urlencode({'address':parks})
+
+#json_data=requests.get(url2).json()
+    #formatted_address=json_data['results'][0]['geometry']['location']
+    #print(parks)
+    #for k,v in formatted_address.items():
+    #print(k,v)
 second_api='https://maps.google.com/maps/api/geocode/json?'
-for parks in cleanlist:
-
+for parks in cleanlist: #unique park names print in new list
     url2=second_api+urllib.parse.urlencode({'address':parks})
+    json_data=requests.get(url2).json()
+    try:
+        formatted_address=json_data['results'][0]['geometry']['location'] #location of the park is displayed
+        print(parks)
+        for k,v in formatted_address.items():
+            print(k,v)
+    except:
+        pass
 
-json_data=requests.get(url2).json()
- try:
-    formatted_address=json_data['results'][0]['geometry']['location']
-    print(parks)
-    for k,v in formatted_address.items():
-    print(k,v)
- except:
-    pass
